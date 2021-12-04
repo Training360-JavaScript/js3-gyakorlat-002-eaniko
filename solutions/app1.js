363f1dea1f5573176952c745dc9deaf5b8c6c087
eaniko <69327188+eaniko@users.noreply.github.com>
2021-12-04 23:06:03 +0100

    végleges2

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,6 +1,6 @@
-const setCokie = (value) => {
+const setCookie = (value) => {
 	const expires = new Date(Date.now() + 15 * 60 * 1000999);
-	document.cookie = 'token=${value};expires=${expires}';
+	document.cookie = `token=${value};expires=${expires}`;
 };
 
 export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,6 +1,21 @@
-const setCokie = (value) => {
-	const expires = new Date(Date.now() + 15 * 60 * 1000999);
-	document.cookie = 'token=${value};expires=${expires}';
-};
+const cookiesArray = document.cookie.split('; ');
+const cookieHandler = {
+	getAll() {
+		return Object.fromEntries(cookiesArray.map((cookie) => cookie.split('=')));
+	},
 
-export { cookieHandler };
+	flush() {
+		cookiesArray.forEach((cookie) => {
+			const [key] = cookie.split('=');
+			document.cookie = `${key}=; expires=Thu, 01 jan 1970 00:00:00 UTC; path=/;`;
+		});
+	},
+
+	toSesssionStorage() {
+		cookiesArray.forEach((cookie) => {
+			const [key, value] = cookie.split('=');
+			sessionStorage.setItem(key, value);
+		});
+	},
+};
+export default cookieHandler;
